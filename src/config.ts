436904b3e4f89238bce3a6fbd8c99bb6f4a03dbe
Nilo Drumond <nilobdrumond@gmail.com>
2022-06-12 10:32:36 -0300

    feat: axios helper setup

---
+++ src/config.ts
@@ -0,0 +1,20 @@
+const API = {
+  BASE_URL: 'https://628bf017667aea3a3e387e51.mockapi.io',
+  LOGIN_URL: '/login',
+  PROFILE_URL: '/me',
+  PRODUCT_URL: '/products',
+  CONVERSIONS_RESUME_URL: '/conversions-resume',
+  USERS_RESUME_URL: '/users-resume',
+  ORDERS_MONTH_URL: '/orders-per-month',
+  CANCELED_ORDERS_MONTH_URL: '/canceled-orders-per-month',
+  PROFIT_EXPECTATION_MONTH_URL: '/profit-expectation-per-month',
+  PROFIT_PER_MONTH_URL: '/profit-per-month',
+  SELLS_MONTH_URL: '/sells-month',
+  ORDER_MONTH_URL: '/orders-month',
+  ALERTS_URL: '/alerts',
+  AVG_TICKET_MONTH: '/avg-ticket-month',
+  AVG_TICKET_DAY: '/avg-ticket/day',
+  CREATE_PRODUCT: '/create-product',
+};
+
+export { API };
---
+++ src/services/axios/types.ts
@@ -0,0 +1,16 @@
+export type HttpResponse<T> = {
+  statusCode: number;
+  body: T;
+};
+
+export type GetParams = {
+  url: string;
+  timeout?: number;
+};
+
+export type PostParams = {
+  url: string;
+  timeout?: number;
+  body?: any;
+  data?: any;
+};
