9be45b0483077d678ff3b34213b830df6ddf8a39
Nilo Drumond <nilobdrumond@gmail.com>
2022-06-14 22:52:19 -0300

    feat: reseting scroll on navigation

--- src/components/AppLayout/index.tsx
+++ src/components/AppLayout/index.tsx
@@ -2,10 +2,20 @@ import React from 'react';
 import { Flex, HStack } from '@chakra-ui/react';
 
 import BackgroundBox from 'components/BackgroundBox';
+import { useLocation } from 'react-router-dom';
 import Sidebar from './components/Sidebar';
 import Header from './components/Header';
 
 const AppLayout: React.FC = ({ children }) => {
+  const { pathname } = useLocation();
+
+  const screenContainerRef = React.useRef<HTMLDivElement>(null);
+
+  // Reset scroll on navigation
+  React.useEffect(() => {
+    if (screenContainerRef.current) screenContainerRef.current.scrollTo(0, 0);
+  }, [pathname]);
+
   return (
     <Flex
       maxW="100vw"
@@ -20,6 +30,7 @@ const AppLayout: React.FC = ({ children }) => {
         <HStack flex={1} alignItems="flex-start" pl={8} spacing={8}>
           <Sidebar />
           <Flex
+            ref={screenContainerRef}
             flex={1}
             py={8}
             h="100%"
